8fefe93c78d3134f18518538f785abc6426856c6
杨林锴 <hmnote36@gmail.com>
2023-03-27 00:41:10 +0800

    Added Pawn class for tank

---
+++ Source/ToonTanks/Pawn/PawnTank.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pawn/PawnClass.h"
+#include "PawnTank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APawnTank : public APawnClass
+{
+	GENERATED_BODY()
+	
+};
