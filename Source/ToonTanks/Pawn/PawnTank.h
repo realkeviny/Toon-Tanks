b6f4105eaa3b8744938677507b457e3149fdc1a8
杨林锴 <hmnote36@gmail.com>
2023-03-27 13:51:40 +0800

    Added Basic Movement for Tank

--- Source/ToonTanks/Pawn/PawnTank.h
+++ Source/ToonTanks/Pawn/PawnTank.h
@@ -19,7 +19,17 @@ private:
 		USpringArmComponent* SpringArm;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 		UCameraComponent* Camera;
+	FVector MoveDirection;
+	FQuat RotationDirection;
 
+	float MoveSpeed = 100.0f;
+	float RotateSpeed = 100.0f;
+
+	void CalculateMoveInput(float Value);
+	void CalculateRotateInput(float Value);
+
+	void Move();
+	void Rotate();
 public:
 	APawnTank();
 
